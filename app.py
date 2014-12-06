42e2498c8be426dee7302fd47ed4be2e55ca6a37
Dan <dan@hostatic.ro>
2014-12-06 16:43:12 +0200

    Added profile page Added about page Adding more stuff to the 'head' and moved it out of the system package Click on row sends you to the profile page

--- app.py
+++ app.py
@@ -47,23 +47,23 @@ def create_app():
     def dosare():
         filtru = request.args.get('filtru')
         if filtru == 'cu_executare':
-            cursor.execute("SELECT nume,prenume,functie_publica,partid_1,fapta,ani_inchisoare,img_url from dosare_corupti where executare=1")
+            cursor.execute("SELECT id,nume,prenume,functie_publica,partid_1,fapta,ani_inchisoare,img_url from dosare_corupti where executare=1")
         elif filtru == 'fara_executare':
-            cursor.execute("SELECT nume,prenume,functie_publica,partid_1,fapta,ani_inchisoare,img_url from dosare_corupti where executare=0")
+            cursor.execute("SELECT id,nume,prenume,functie_publica,partid_1,fapta,ani_inchisoare,img_url from dosare_corupti where executare=0")
         elif filtru == 'nume':
-            cursor.execute("SELECT nume,prenume,functie_publica,partid_1,fapta,ani_inchisoare,img_url from dosare_corupti order by nume")
+            cursor.execute("SELECT id,nume,prenume,functie_publica,partid_1,fapta,ani_inchisoare,img_url from dosare_corupti order by nume")
         elif filtru == 'functie_publica':
-            cursor.execute("SELECT nume,prenume,functie_publica,partid_1,fapta,ani_inchisoare,img_url from dosare_corupti order by functie_publica")
+            cursor.execute("SELECT id,nume,prenume,functie_publica,partid_1,fapta,ani_inchisoare,img_url from dosare_corupti order by functie_publica")
         elif filtru == 'partid':
-            cursor.execute("SELECT nume,prenume,functie_publica,partid_1,fapta,ani_inchisoare,img_url from dosare_corupti order by partid_1")
+            cursor.execute("SELECT id,nume,prenume,functie_publica,partid_1,fapta,ani_inchisoare,img_url from dosare_corupti order by partid_1")
         elif filtru == 'fapta':
-            cursor.execute("SELECT nume,prenume,functie_publica,partid_1,fapta,ani_inchisoare,img_url from dosare_corupti order by fapta")
+            cursor.execute("SELECT id,nume,prenume,functie_publica,partid_1,fapta,ani_inchisoare,img_url from dosare_corupti order by fapta")
         elif filtru == 'data_sentinta':
-            cursor.execute("SELECT nume,prenume,functie_publica,partid_1,fapta,ani_inchisoare,img_url from dosare_corupti order by data_condamnarii")
+            cursor.execute("SELECT id,nume,prenume,functie_publica,partid_1,fapta,ani_inchisoare,img_url from dosare_corupti order by data_condamnarii")
         elif filtru == 'durata_dosar':
-            cursor.execute("SELECT nume,prenume,functie_publica,partid_1,fapta,ani_inchisoare,img_url from dosare_corupti order by durata_dosar")
+            cursor.execute("SELECT id,nume,prenume,functie_publica,partid_1,fapta,ani_inchisoare,img_url from dosare_corupti order by durata_dosar")
         elif filtru == 'ani_pedeapsa':
-            cursor.execute("SELECT nume,prenume,functie_publica,partid_1,fapta,ani_inchisoare,img_url from dosare_corupti order by ani_inchisoare")
+            cursor.execute("SELECT id,nume,prenume,functie_publica,partid_1,fapta,ani_inchisoare,img_url from dosare_corupti order by ani_inchisoare")
         dosare = cursor.fetchall()
 
         # Send all the variables to the template
@@ -72,6 +72,18 @@ def create_app():
             dosare=dosare
             )
         cursor.close ()
+
+    @app.route('/profil')
+    def profil():
+        return render_template(
+            'profil.html'
+            )
+
+    @app.route('/despre')
+    def despre():
+        return render_template(
+            'despre.html'
+            )
 
     # Return the html
     return app
