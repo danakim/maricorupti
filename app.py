35e1102f589f88418638dfc51a94db186868468e
Dan <dan@hostatic.ro>
2015-01-02 22:07:19 +0200

    Added the search box + the search functionality. Also the search box is a real-time filter in the 'dosare' page. The table headers are also sortable now.

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 from flaskext.mysql import MySQL
 from datetime import date
 import re
@@ -22,20 +22,23 @@ def create_app():
 
     @app.route('/')
     def home_page():
-        cursor.execute("SELECT MAX(data_condamnarii) from dosare_corupti")
-        data_condamnarii = cursor.fetchone()
-        today = date.today()
-        ultima_condamnare = today - data_condamnarii[0]
-        cursor.execute("SELECT COUNT(id) from dosare_corupti")
-        total_dosare = cursor.fetchone()
-        cursor.execute("SELECT COUNT(id) from dosare_corupti where executare=1")
-        total_dosare_cu_executare = cursor.fetchone()
-        cursor.execute("SELECT COUNT(id) from dosare_corupti where executare=0")
-        total_dosare_fara_executare = cursor.fetchone()
-        cursor.execute("SELECT MAX(ani_inchisoare) from dosare_corupti")
-        pedeapsa_maxima = cursor.fetchone()
-        cursor.execute("SELECT MAX(durata_dosar) from dosare_corupti")
-        dosar_maxim = cursor.fetchone()
+        if request.args.get('filtru'):
+            return redirect("/dosare?filtru=" + request.args.get('filtru'))
+        else:
+            cursor.execute("SELECT MAX(data_condamnarii) from dosare_corupti")
+            data_condamnarii = cursor.fetchone()
+            today = date.today()
+            ultima_condamnare = today - data_condamnarii[0]
+            cursor.execute("SELECT COUNT(id) from dosare_corupti")
+            total_dosare = cursor.fetchone()
+            cursor.execute("SELECT COUNT(id) from dosare_corupti where executare=1")
+            total_dosare_cu_executare = cursor.fetchone()
+            cursor.execute("SELECT COUNT(id) from dosare_corupti where executare=0")
+            total_dosare_fara_executare = cursor.fetchone()
+            cursor.execute("SELECT MAX(ani_inchisoare) from dosare_corupti")
+            pedeapsa_maxima = cursor.fetchone()
+            cursor.execute("SELECT MAX(durata_dosar) from dosare_corupti")
+            dosar_maxim = cursor.fetchone()
 
         # Send all the variables to the template
         return render_template(
@@ -56,20 +59,10 @@ def create_app():
             cursor.execute("SELECT id,nume,prenume,functie_publica,partid_1,fapta,ani_inchisoare,img_url from dosare_corupti where executare=1")
         elif filtru == 'fara_executare':
             cursor.execute("SELECT id,nume,prenume,functie_publica,partid_1,fapta,ani_inchisoare,img_url from dosare_corupti where executare=0")
-        elif filtru == 'nume':
-            cursor.execute("SELECT id,nume,prenume,functie_publica,partid_1,fapta,ani_inchisoare,img_url from dosare_corupti order by nume")
-        elif filtru == 'functie_publica':
-            cursor.execute("SELECT id,nume,prenume,functie_publica,partid_1,fapta,ani_inchisoare,img_url from dosare_corupti order by functie_publica")
-        elif filtru == 'partid':
-            cursor.execute("SELECT id,nume,prenume,functie_publica,partid_1,fapta,ani_inchisoare,img_url from dosare_corupti order by partid_1")
-        elif filtru == 'fapta':
-            cursor.execute("SELECT id,nume,prenume,functie_publica,partid_1,fapta,ani_inchisoare,img_url from dosare_corupti order by fapta")
-        elif filtru == 'data_sentinta':
-            cursor.execute("SELECT id,nume,prenume,functie_publica,partid_1,fapta,ani_inchisoare,img_url from dosare_corupti order by data_condamnarii")
-        elif filtru == 'durata_dosar':
-            cursor.execute("SELECT id,nume,prenume,functie_publica,partid_1,fapta,ani_inchisoare,img_url from dosare_corupti order by durata_dosar")
-        elif filtru == 'ani_pedeapsa':
-            cursor.execute("SELECT id,nume,prenume,functie_publica,partid_1,fapta,ani_inchisoare,img_url from dosare_corupti order by ani_inchisoare")
+        elif filtru in ['nume', 'functie_publica', 'partid_1', 'fapta', 'data_condamnarii', 'durata_dosar', 'ani_inchisoare']:
+            cursor.execute("SELECT id,nume,prenume,functie_publica,partid_1,fapta,ani_inchisoare,img_url from dosare_corupti order by " + filtru)
+        else:
+            cursor.execute("SELECT id,nume,prenume,functie_publica,partid_1,fapta,ani_inchisoare,img_url from dosare_corupti WHERE (nume like '%" + filtru + "%' or prenume like '%" + filtru + "%' or functie_publica like '%" + filtru + "%' or partid_1 like '%" + filtru + "%' or fapta like '%" + filtru + "%') order by nume")
         dosare = cursor.fetchall()
 
         # Send all the variables to the template
